2d3bb8f1a56ae19a1cf86b01a64cfe595ac333d6
Leissan Khan <leissan.khan@gmail.com>
2023-02-25 14:14:43 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   
   resources :users
   resources :logs
-  resources :exercises
+  resources :exercises, only: [:index, :show]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  #get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+ 
 end
